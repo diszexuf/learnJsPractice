401f9a4adb5a418a5ee6a9ef3d4c60a95dbbf1d9
Алексей Щелканов <superagen228@gmail.com>
2024-02-11 12:04:22 +0500

    aaa

---
+++ 03-theory-num-str-date/01-numbers.js
@@ -0,0 +1,14 @@
+const intNum = 42
+const floatNum = 42.42
+const powNum = 10e3
+const bigNum = 1_000_000
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Math.pow(2, 53) - 1)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.POSITIVE_INFINITY)
+console.log(Number.NEGATIVE_INFINITY)
+console.log(1 / 0)
+console.log(Number.isFinite(Infinity))
