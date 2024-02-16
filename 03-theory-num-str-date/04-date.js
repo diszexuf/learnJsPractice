bca99be875d20fb9a74a77ca1450de5e6f9b9a75
diszexuf <superagen228@gmail.com>
2024-02-16 23:02:38 +0500

    6:01:40

--- 03-theory-num-str-date/04-date.js
+++ 03-theory-num-str-date/04-date.js
@@ -21,10 +21,42 @@
 
 // ===============
 
-const now = new Date()
+let mode = 'full'
 const output = document.getElementById('output')
-const fullbtn = document.getElementById('full')
+const fullBtn = document.getElementById('full')
 const dateBtn = document.getElementById('date')
 const timeBtn = document.getElementById('time')
 
-output.textContent = 'asd'
+function bindMode(name) {
+	return function () {
+		mode = name
+		update()
+	}
+}
+
+fullBtn.onclick = bindMode('full')
+
+dateBtn.onclick = bindMode('date')
+
+timeBtn.onclick = bindMode('time')
+
+setInterval(update, 500)
+update()
+
+function update() {
+	output.textContent = format(mode)
+}
+
+function format(formatMode) {
+	const now = new Date()
+	switch (formatMode) {
+		case 'time':
+			return now.toLocaleTimeString()
+		case 'date':
+			return now.toLocaleDateString()
+		case 'full':
+			return now.toLocaleDateString() + ' ' + now.toLocaleTimeString()
+		default:
+			return
+	}
+}
